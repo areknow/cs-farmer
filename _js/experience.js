5fb45305bbc7eb18b1bcda4b9a59153d83076486
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-07-09 16:42:25 -0400

    experience page

--- _js/experience.js
+++ _js/experience.js
@@ -1,5 +1,5 @@
 // ====================================================================
-// About - js
+// Experience - js
 // 
 // Scripts for the about page
 // ====================================================================
@@ -10,6 +10,37 @@
   // Document ready
   // ============================================
   $(function() {
+
+    // Default options for sliders
+    owlOptions = {
+      items: 1,
+      dots: false,
+      loop: true
+    }
+
+    // Healthcare slider
+    var healthCareSlider = $('.healthcare-slider .owl-carousel');
+    healthCareSlider.owlCarousel(owlOptions);
+    $('.healthcare-slider .prev').click(function() { healthCareSlider.trigger('prev.owl.carousel'); });
+    $('.healthcare-slider .next').click(function() { healthCareSlider.trigger('next.owl.carousel'); });
+
+    // Hospitality slider
+    var hospitalitySlider = $('.hospitality-slider .owl-carousel');
+    hospitalitySlider.owlCarousel(owlOptions);
+    $('.hospitality-slider .prev').click(function() { hospitalitySlider.trigger('prev.owl.carousel'); });
+    $('.hospitality-slider .next').click(function() { hospitalitySlider.trigger('next.owl.carousel'); });
+
+    // Commercial slider
+    var commercialSlider = $('.commercial-slider .owl-carousel');
+    commercialSlider.owlCarousel(owlOptions);
+    $('.commercial-slider .prev').click(function() { commercialSlider.trigger('prev.owl.carousel'); });
+    $('.commercial-slider .next').click(function() { commercialSlider.trigger('next.owl.carousel'); });
+
+    // Other slider
+    var otherSlider = $('.other-slider .owl-carousel');
+    otherSlider.owlCarousel(owlOptions);
+    $('.other-slider .prev').click(function() { otherSlider.trigger('prev.owl.carousel'); });
+    $('.other-slider .next').click(function() { otherSlider.trigger('next.owl.carousel'); });
 
   });// end document ready
 
--- _js/home.js
+++ _js/home.js
@@ -13,14 +13,16 @@
 
     var owl = $('.owl-carousel');
     owl.owlCarousel({
-      items: 1
+      items: 1,
+      dots: false,
+      loop: true
     });
     $('.slider-nav .prev').click(function() {
       owl.trigger('prev.owl.carousel');
-    })
+    });
     $('.slider-nav .next').click(function() {
       owl.trigger('next.owl.carousel');
-    })
+    });
 
   });// end document ready
 
